3b30241c431cf42e23b79138c34343a1ce47f392
Juan Pablo Garza <juanp@juanpgarza.com>
2024-12-08 16:09:10 +0000

    [MIG] stock_picking_deliver_to: Migration to 17.0

--- stock_picking_deliver_to/__manifest__.py
+++ stock_picking_deliver_to/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "stock_picking_deliver_to",
     "summary": "",
-    "version": "15.0.2.0.0",
+    "version": "17.0.1.0.0",
     "category": "Warehouse Management",
     "website": "https://github.com/juanpgarza/stock-addons",
     "author": "juanpgarza",
@@ -17,5 +17,5 @@
         'views/stock_picking_views.xml',
         'views/report_deliveryslip.xml',
         ],
-    "installable": False,
+    "installable": True,
 }
