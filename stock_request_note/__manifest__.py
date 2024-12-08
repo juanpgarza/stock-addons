76daecdcff1995563409f23d5329e6f88cb4eadd
Juan Pablo Garza <juanp@juanpgarza.com>
2024-12-08 16:09:10 +0000

    [MIG] stock_request_note: Migration to 17.0

--- stock_request_note/__manifest__.py
+++ stock_request_note/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "stock_request_note",
     "summary": "",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Warehouse Management",
     "website": "https://github.com/juanpgarza/stock-addons",
     "author": "juanpgarza",
@@ -17,5 +17,5 @@
         'views/stock_request_views.xml',
         'views/stock_picking_views.xml',        
         ],
-    "installable": False,
+    "installable": True,
 }
