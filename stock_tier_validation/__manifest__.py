f07dcaad0de8fca74447e8b4a4bcc56d863acb54
Juan Pablo Garza <juanp@juanpgarza.com>
2024-12-08 16:09:10 +0000

    [MIG] stock_tier_validation: Migration to 17.0

--- stock_tier_validation/__manifest__.py
+++ stock_tier_validation/__manifest__.py
@@ -4,13 +4,13 @@
     "name": "Stock Tier Validation",
     "summary": "Extends the functionality of Stock Pickings to "
                "support a tier validation process.",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Inventory",
     "website": "https://badep.ma",
     "author": "BADEP, Open Source Integrators, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
-    "installable": False,
+    "installable": True,
     "depends": [
         "stock",
         "base_tier_validation",
