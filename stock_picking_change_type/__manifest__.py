218126b84c7e97f347f116658db28003d490b71a
Juan Pablo Garza <juanp@juanpgarza.com>
2024-12-08 16:09:10 +0000

    [MIG] stock_picking_change_type: Migration to 17.0

--- stock_picking_change_type/__manifest__.py
+++ stock_picking_change_type/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "stock_picking_change_type",
     "summary": "",
-    "version": "15.0.2.0.0",
+    "version": "17.0.1.0.0",
     "category": "Stock",
     "website": "https://github.com/juanpgarza/stock-addons",
     "author": "juanpgarza",
@@ -15,5 +15,5 @@
         # 'views/stock_picking_views.xml',        
         'wizards/change_picking_type_views.xml',
         ],
-    "installable": False,
+    "installable": True,
 }
--- stock_picking_change_type/wizards/__init__.py
+++ stock_picking_change_type/wizards/__init__.py
@@ -1,3 +1,2 @@
-# -*- coding: utf-8 -*-
 
 from . import change_picking_type
