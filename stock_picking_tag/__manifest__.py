882c128ae083b1629b104bfe87fdee6e80f677a1
Juan Pablo Garza <juanp@juanpgarza.com>
2025-06-14 20:26:22 -0300

    [MIG] stock_picking_tag: Migration to 18.0

--- stock_picking_tag/__manifest__.py
+++ stock_picking_tag/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "stock_picking_tag",
     "summary": "",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Stock",
     "website": "https://github.com/juanpgarza/stock-addons",
     "author": "juanpgarza",
@@ -15,5 +15,5 @@
         'views/stock_picking_tag_views.xml',        
         'security/ir.model.access.csv',
         ],
-    "installable": False,
+    "installable": True,
 }
