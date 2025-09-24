1797e71d4ac5b8ddc18bf98e61f41c412d00fbd3
Juan Pablo Garza <juanp@juanpgarza.com>
2025-09-24 12:38:02 -0300

    FIX stock_picking_return_reason

--- stock_picking_return_reason/wizards/stock_return_picking.py
+++ stock_picking_return_reason/wizards/stock_return_picking.py
@@ -8,10 +8,17 @@ class StockReturnPicking(models.TransientModel):
 
     reason_id = fields.Many2one(comodel_name="stock.return.picking.reason", string= 'Motivo de devolución')
 
-    def _create_returns(self):
-        # add to new picking for return the reason for the return
-        new_picking, pick_type_id = super()._create_returns()
-        picking = self.env['stock.picking'].browse(new_picking)
-        picking.write({'reason_id': self.reason_id.id})
+    # def _create_returns(self):
+    #     # add to new picking for return the reason for the return
+    #     new_picking, pick_type_id = super()._create_returns()
+    #     picking = self.env['stock.picking'].browse(new_picking)
+    #     picking.write({'reason_id': self.reason_id.id})
 
-        return new_picking, pick_type_id
+    #     return new_picking, pick_type_id
+
+    def _create_return(self):
+        # add to new picking for return the reason for the return
+        new_picking = super()._create_return()
+        new_picking.write({'reason_id': self.reason_id.id})
+        # import pdb; pdb.set_trace()
+        return new_picking
